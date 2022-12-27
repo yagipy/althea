162e0e6860ef5e022df69d11bd2f355643b79718
yagipy <yo@yagipy.me>
2022-12-28 05:10:21 +0900

    Allow http

--- doc/reference/benchmark-server/app/go.mod
+++ doc/reference/benchmark-server/app/go.mod
@@ -1,3 +1,5 @@
 module app
 
 go 1.19
+
+require github.com/coreos/go-systemd v0.0.0-20191104093116-d3cd4ed1dbcf
--- doc/reference/benchmark-server/infra/server.tf
+++ doc/reference/benchmark-server/infra/server.tf
@@ -19,7 +19,7 @@ resource "aws_instance" "api" {
   instance_type          = "t3.micro"
   iam_instance_profile = aws_iam_instance_profile.deployer.name
   subnet_id              = aws_subnet.public.id
-#  vpc_security_group_ids = [aws_security_group.allow_http.id]
+  vpc_security_group_ids = [aws_security_group.allow_http.id]
 }
 
 resource "aws_eip" "api" {
@@ -34,6 +34,7 @@ resource "aws_security_group" "allow_http" {
     from_port       = 80
     to_port         = 80
     protocol        = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
     from_port   = 0
