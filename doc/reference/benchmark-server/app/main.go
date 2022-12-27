947ff444984ebdd8585150b1eb068b97a7f2eb72
yagipy <yo@yagipy.me>
2022-12-28 05:10:21 +0900

    Allow http

--- doc/reference/benchmark-server/app/main.go
+++ doc/reference/benchmark-server/app/main.go
@@ -2,12 +2,29 @@ package main
 
 import (
 	"fmt"
+	"github.com/coreos/go-systemd/activation"
+	"log"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello")
 	})
-	http.ListenAndServe(":8081", nil)
+	server := &http.Server{
+		Handler: mux,
+	}
+
+	listener, err := activation.Listeners()
+	if err != nil {
+		log.Printf("%s\n", err)
+		return
+	}
+	if len(listener) == 0 {
+		log.Printf("can't find any listeners\n")
+		return
+	}
+
+	server.Serve(listener[0])
 }
